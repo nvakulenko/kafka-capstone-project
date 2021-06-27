3935a09be7a6910875197521589638c85692005b
NataliiaVakulenkoExtern <nataliia.vakulenko.extern@solarisbank.de>
2021-06-27 18:22:13 +0300

    Fix service

--- sentiment-detection-service/src/main/java/ua/edu/ucu/SentimentDetectionApplication.java
+++ sentiment-detection-service/src/main/java/ua/edu/ucu/SentimentDetectionApplication.java
@@ -2,36 +2,37 @@ package ua.edu.ucu;
 
 import com.azure.ai.textanalytics.TextAnalyticsClient;
 import com.azure.ai.textanalytics.TextAnalyticsClientBuilder;
-import com.azure.ai.textanalytics.models.DetectedLanguage;
 import com.azure.ai.textanalytics.models.DocumentSentiment;
 import com.azure.core.credential.AzureKeyCredential;
-import org.apache.kafka.clients.admin.Admin;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SentimentDetectionApplication {
-    public static void main(String args[])
-    {
+
+    private static final Logger logger = LoggerFactory
+        .getLogger(SentimentDetectionApplication.class.getName());
+
+    public static void main(String args[]) {
         TextAnalyticsClient textAnalyticsClient = new TextAnalyticsClientBuilder()
-                .credential(new AzureKeyCredential("48bccf3b94f945d2a837ed994adebe30"))
-                .endpoint("https://solomiya-text-api.cognitiveservices.azure.com/")
-                .buildClient();
+            .credential(new AzureKeyCredential(""))
+            .endpoint("https://solomiya-text-api.cognitiveservices.azure.com/")
+            .buildClient();
 
         Properties props_consumer = new Properties();
         props_consumer.put("bootstrap.servers", "broker:9092");
-        props_consumer.put("group.id", "MyCounter");
+        props_consumer.put("group.id", "sentiments");
         props_consumer.put("key.deserializer",
             "org.apache.kafka.common.serialization.StringDeserializer");
         props_consumer.put("value.deserializer",
@@ -67,27 +68,36 @@ public class SentimentDetectionApplication {
         Duration timeout = Duration.ofMillis(100);
 
     while (true) {
-        ConsumerRecords<String, String> records = consumer.poll(timeout);
-        for (ConsumerRecord<String, String> record : records) {
+        try {
+            ConsumerRecords<String, String> records = consumer.poll(timeout);
+            for (ConsumerRecord<String, String> record : records) {
 
-            DocumentSentiment documentSentiment = textAnalyticsClient.analyzeSentiment(record.value());
+                DocumentSentiment documentSentiment = textAnalyticsClient
+                    .analyzeSentiment(record.value());
 
-            System.out.printf(record.value().toString());
-            System.out.printf("\nAnalyzed document sentiment: %s.%n", documentSentiment.getSentiment());
+                logger.info("Key: " + record.key() + ", Value:" + record.value());
+                logger.info("Partition:" + record.partition() + ",Offset:" + record.offset());
+                logger.info("\nAnalyzed document sentiment: %s.%n",
+                    documentSentiment.getSentiment());
 
-            ProducerRecord<String, String> new_record =
-                    new ProducerRecord<>("sentiment-out", record.key().toString(), documentSentiment.getSentiment().toString());
-            try {
-                producer.send(new_record).get();
-                producer.flush();
-                producer.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+                ProducerRecord<String, String> new_record =
+                    new ProducerRecord<>("sentiment-out", record.key(),
+                        documentSentiment.getSentiment().toString());
+                try {
+                    logger.info("Send: " + new_record);
+
+                    producer.send(new_record);
+                    producer.flush();
+
+                } catch (Exception e) {
+                    logger.error(e.getLocalizedMessage(), e);
+                }
+                logger.info("done");
             }
-            System.out.printf("done");
-
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage(), e);
         }
     }
-
+        // producer.close();
     }
 }
