7231fca37d556f7b61110682ee6ca9fbd6a9563f
avedilin <superflur@gmail.com>
2021-06-30 17:16:35 +0300

    - add docker - add RAKE algorithm

---
+++ keyword-detection-service/README.md
@@ -0,0 +1,3 @@
+## Keywords service
+
+- To generate list of commands for Dockefile run ```sbt> show dockerCommands```
--- keyword-detection-service/src/main/scala/ua/edu/ucu/Main.scala
+++ keyword-detection-service/src/main/scala/ua/edu/ucu/Main.scala
@@ -3,10 +3,12 @@ package ua.edu.ucu
 object Main extends App {
 //  createTopic()
 
-  KeywordDetectionService(
-    "localhost:29092",
-    "http://localhost:8081",
-    "keyword-detection-service",
-    "write-topic"
-  )
+//  KeywordDetectionService(
+//    "localhost:29092",
+//    "http://localhost:8081",
+//    "keyword-detection-service",
+//    "write-topic"
+//  )
+
+  println(RAKE.run("It's pretty well known and it was a paid product placement. Hamilton advertised the watch around the movie and had an Interstellar page on their website. The biggest product placement was the Carhartt jacket though.   Brands send old merch all the time by the way for movies. Just watch Stranger Things for tons of examples of that. Eggos are prominently featured as Eleven's favorite food"))
 }
